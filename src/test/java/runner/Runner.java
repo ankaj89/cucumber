c0a997f0e8af877f22ee575c230f359874c48868
ankaj89 <45896847+ankaj89@users.noreply.github.com>
2018-12-16 11:41:44 +0530

    Update Runner.java
    changes tag

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -3,7 +3,7 @@ package runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = { "src/test/resources/features" },glue = "steps",tags={"@selenium"})
+@CucumberOptions(features = { "src/test/resources/features" },glue = "steps",tags={"@mobile"})
 public class Runner extends AbstractTestNGCucumberTests {
 
 }
